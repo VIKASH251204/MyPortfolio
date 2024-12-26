99b71112e58087fd1f88c5246015a7f98217491e
VIKASH251204 <vikashtailor2512@gmail.com>
2024-12-26 11:34:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,17 +2,21 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let menu = document.querySelector('.menu');
 let list = document.querySelectorAll('.list');
+let button = document.querySelector('.button');
 
 
 
 
-menuIcon.addEventListener("click", () => {
-    menu.style.display='block';
-  });
+menuIcon.addEventListener('click', function() {
+  if (menu.style.display === 'none' || menu.style.display === '') {
+      menu.style.display = 'block';
+  } else {
+      menu.style.display = 'none';
+  }
+});
+
   
-  menu.showPopover();
-  menu.hidePopover();
-  // `toggle` only fires once
+  
 
 
 
